eb6a2e7169ea893f6a3cac37c42d0c4beb0f03fa
webeweb <support@webeweb.fr>
2018-04-16 14:38:13 +0200

    Add Abstract FTP client

---
+++ Client/AbstractFTPClient.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * This file is part of the ftp-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\FTP\Client;
+
+use WBW\Library\Core\Security\Authenticator;
+use WBW\Library\FTP\Exception\FTPException;
+
+/**
+ * Abstract FTP client.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\FTP\Client
+ * @abstract
+ */
+abstract class AbstractFTPClient {
+
+    /**
+     * Authenticator.
+     *
+     * @var Authenticator
+     */
+    private $authenticator;
+
+    /**
+     * Connection.
+     *
+     * @var ressource
+     */
+    private $connection;
+
+    /**
+     * Constructor.
+     *
+     * @param Authenticator $authenticator The authenticator.
+     */
+    protected function __construct(Authenticator $authenticator) {
+        $this->authenticator = $authenticator;
+    }
+
+    /**
+     * Get the authenticator.
+     *
+     * @return Authenticator Returns the authenticator.
+     */
+    final public function getAuthenticator() {
+        return $this->authenticator;
+    }
+
+    /**
+     * Get the connection.
+     *
+     * @return resource Returns the connection.
+     */
+    final public function getConnection() {
+        return $this->connection;
+    }
+
+    /**
+     *
+     * @param string $message The message.
+     */
+    final protected function newFTPException($message) {
+        return new FTPException(sprintf("%s://%s:%s@%s:%d " . $message, $this->authenticator->getScheme(), $this->authenticator->getPasswordAuthentication()->getUsername(), $this->authenticator->getPasswordAuthentication()->getPassword(), $this->authenticator->getHost(), $this->authenticator->getPort()));
+    }
+
+    /**
+     * Set the authenticator.
+     *
+     * @param \WBW\Library\FTP\Client\Authenticator $authenticator The authenticator.
+     * @returns AbstractFTPClient Returns this abstract FTP client.
+     */
+    final protected function setAuthenticator(Authenticator $authenticator) {
+        $this->authenticator = $authenticator;
+        return $this;
+    }
+
+    /**
+     * Set the connection.
+     *
+     * @param ressource $connection The connection.
+     * @returns AbstractFTPClient Returns this abstract FTP client.
+     */
+    final protected function setConnection($connection) {
+        $this->connection = $connection;
+        return $this;
+    }
+
+}
