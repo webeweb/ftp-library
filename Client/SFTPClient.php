afa6c93335195caebf7aa0785d697e3d98768930
webeweb <support@webeweb.fr>
2018-04-17 16:53:10 +0200

    Implements FTP methods into SFTP client

--- Client/SFTPClient.php
+++ Client/SFTPClient.php
@@ -23,6 +23,13 @@ use WBW\Library\FTP\Exception\FTPException;
 class SFTPClient extends AbstractFTPClient {
 
     /**
+     * SFTP resource.
+     *
+     * @var mixed
+     */
+    private $sftp;
+
+    /**
      * Constructor.
      *
      * @param Authenticator $authenticator The authenticator.
@@ -36,7 +43,20 @@ class SFTPClient extends AbstractFTPClient {
     }
 
     /**
-     * Opens this FTP connection.
+     * Closes this SFTP connection.
+     *
+     * @return SFTPClient Returns this SFTP client.
+     * @throws FTPException Throws a FTP exception if an I/O error occurs.
+     */
+    public function close() {
+        if (false === ssh2_exec($this->getConnection(), "exit")) {
+            throw $this->newFTPException("close failed");
+        }
+        return $this;
+    }
+
+    /**
+     * Opens this SFTP connection.
      *
      * @return SFTPClient Returns this SFTP client.
      * @throws FTPException Throws a FTP exception if an I/O error occurs.
@@ -44,7 +64,7 @@ class SFTPClient extends AbstractFTPClient {
     public function connect() {
         $host = $this->getAuthenticator()->getHost();
         $port = $this->getAuthenticator()->getPort();
-        $this->setConnection(ssh2_ssl_connect($host, $port));
+        $this->setConnection(ssh2_connect($host, $port));
         if (false === $this->getConnection()) {
             throw $this->newFTPException("connection failed");
         }
@@ -52,7 +72,33 @@ class SFTPClient extends AbstractFTPClient {
     }
 
     /**
-     * Logs in to this FTP connection.
+     * Deletes a file on the SFTP server.
+     *
+     * @param string $path The file to delete.
+     * @return SFTPClient Returns this SFTP client.
+     * @throws FTPException Throws a FTP exception if an I/O error occurs.
+     */
+    public function delete($path) {
+        if (false === ssh2_sftp_unlink($this->getSFTP(), $path)) {
+            throw $this->newFTPException(sprintf("delete %s failed", $path));
+        }
+        return $this;
+    }
+
+    /**
+     * Get the SFTP resource.
+     *
+     * @return mixed Returns the SFTP resource.
+     */
+    private function getSFTP() {
+        if (null === $this->sftp) {
+            $this->sftp = ssh2_sftp($this->getConnection());
+        }
+        return $this->sftp;
+    }
+
+    /**
+     * Logs in to this SFTP connection.
      *
      * @return SFTPClient Returns this SFTP client.
      * @throws FTPException Throws a FTP exception if an I/O error occurs.
@@ -62,6 +108,67 @@ class SFTPClient extends AbstractFTPClient {
         $password = $this->getAuthenticator()->getPasswordAuthentication()->getPassword();
         if (false === ssh2_auth_password($this->getConnection(), $username, $password)) {
             throw $this->newFTPException("login failed");
+        }
+        return $this;
+    }
+
+    /**
+     * Creates a directory.
+     *
+     * @param string $directory The directory.
+     * @param integer $mode The mode.
+     * @param boolean $recursive Recursively ?.
+     * @return SFTPClient Returns this SFTP client.
+     * @throws FTPException Throws a FTP exception if an I/O error occurs.
+     */
+    public function mkdir($directory, $mode = 0777, $recursive = false) {
+        if (false === ssh2_sftp_mkdir($this->getSFTP(), $directory, $mode, $recursive)) {
+            throw $this->newFTPException(sprintf("mkdir %s failed", $directory));
+        }
+        return $this;
+    }
+
+    /**
+     * Uploads a file to the SFTP server.
+     *
+     * @param string $localFile The local file.
+     * @param string $remoteFile The remote file.
+     * @param integer $createMode The create mode.
+     * @return SFTPClient Returns this SFTP client.
+     * @throws FTPException Throws a FTP exception if an I/O error occurs.
+     */
+    public function put($localFile, $remoteFile) {
+        $stream = fopen("ssh2.sftp://" . intval($this->getSFTP()) . $remoteFile, "w");
+        fwrite($stream, file_get_contents($localFile));
+        fclose($stream);
+        return $this;
+    }
+
+    /**
+     * Renames a file or a directory on the SFTP server.
+     *
+     * @param string $oldName The old file/directory name.
+     * @param string $newName The new name.
+     * @return SFTPClient Returns this SFTP client.
+     * @throws FTPException Throws a FTP exception if an I/O error occurs.
+     */
+    public function rename($oldName, $newName) {
+        if (false === ssh2_sftp_rename($this->getSFTP(), $oldName, $newName)) {
+            throw $this->newFTPException(sprintf("rename %s into %s failed", $oldName, $newName));
+        }
+        return $this;
+    }
+
+    /**
+     * Removes a directory.
+     *
+     * @param string $directory The directory.
+     * @return SFTPClient Returns this SFTP client.
+     * @throws FTPException Throws a FTP exception if an I/O error occurs.
+     */
+    public function rmdir($directory) {
+        if (false === ssh2_sftp_rmdir($this->getSFTP(), $directory)) {
+            throw $this->newFTPException(sprintf("rmdir %s failed", $directory));
         }
         return $this;
     }
