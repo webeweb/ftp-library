06f8a87156fe2dc62c0feff28eb17c5a9fde4764
webeweb <support@webeweb.fr>
2018-08-06 13:40:27 +0200

    Rename testConctructor() methods

--- Tests/Client/FTPClientTest.php
+++ Tests/Client/FTPClientTest.php
@@ -75,7 +75,7 @@ final class FTPClientTest extends PHPUnit_Framework_TestCase {
      *
      * @return void
      */
-    public function testConstructor() {
+    public function testConstruct() {
 
         $obj = new FTPClient($this->authenticatorW);
 
--- Tests/Exception/FTPExceptionTest.php
+++ Tests/Exception/FTPExceptionTest.php
@@ -27,7 +27,7 @@ final class FTPExceptionTest extends PHPUnit_Framework_TestCase {
      *
      * @return void
      */
-    public function testConstructor() {
+    public function testConstruct() {
 
         $obj = new FTPException("exception");
 
